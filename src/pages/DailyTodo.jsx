7d3e7fddda094de0743df88c7edd70e3d37c995b
JellyBear97 <olxloha@gamil.com>
2023-07-16 22:38:54 +0900

    Feat : home에서 데이터 조회기능 추가, 현재 날짜별 todolist 상세보기 조회중
    - 날짜별 todolist 데이터 조회중 오류 발생. home에서와 다른 점이라고는 api 주소뿐이고, 주소를 검색창에 불러왔을 때 잘 불러오지만 const {data, isLoading, isError} = useQuery('oneDayTodo', async () => {}) 어쩌구로 불러올때는 data가 안찍히고, axios로 get한 걸 response라는 데이터에 따로 저장해서 return response.data해줘야 나옴.. 왜그런지 궁금

--- src/pages/DailyTodo.jsx
+++ src/pages/DailyTodo.jsx
@@ -1,6 +1,29 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
 
 const DailyTodo = () => {
+  const { date } = useParams();
+  // console.log(date);
+  const { data, isLoading, isError } = useQuery('oneDayTodo', async () => {
+    const response = await axios.get(`http://localhost:4001/todos?userId=1&&date=${date}`);
+    // return response.data;
+  });
+  console.log('isLoading', isLoading);
+  if (isLoading) {
+    return <div>로딩중</div>;
+  }
+  console.log('isError', isError);
+
+  if (isError) {
+    return <div>에러남</div>;
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <>
       <h2>날짜별 조회</h2>
